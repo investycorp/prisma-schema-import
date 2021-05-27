0af1c3926b66994176c6515035c08392fe178231
Chan Kang <me@kentakang.com>
2021-05-27 10:50:12 +0900

    feat: add load schema dependencies

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
+const chalk = require('chalk');
+const path = require('path');
 
-const { program } = require('commander');
+const packageJsonPath = path.join(process.cwd(), 'package.json');
+const packageJson = require(packageJsonPath);
+const localDependencies = packageJson.dependencies ? Object.keys(packageJson.dependencies) : [];
+const devDependencies = packageJson.devDependencies ? Object.keys(packageJson.devDependencies) : [];
+const dependencies = localDependencies.concat(devDependencies);
+const schemaDependencies = dependencies.filter(dependency => !dependency.includes('prisma-schema-import') && dependency.includes('prisma-schema-'));
 
-console.log(Object.keys(require(__dirname + '/package.json').dependencies));
+if (schemaDependencies.length) {
+    const schemaName = schemaDependencies[0];
+    const schema = require(schemaDependencies[0]);
+    
+    console.log('schema', schema());
+    console.log(chalk.keyword('green')(`${schemaName} is loaded!`));
+} else {
+    console.log(chalk.keyword('orange')('Prisma schema dependencies are not detected'));
+}
