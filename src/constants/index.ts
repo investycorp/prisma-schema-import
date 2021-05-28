218959235868b24cf1f1e9f87ff70289392a008a
Chan Kang <me@kentakang.com>
2021-05-28 18:15:58 +0900

    fix: remove SCHEMA_SCRIPT constant

--- src/constants/index.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line quotes
-const SCHEMA_SCRIPT = `const fs = require('fs');\n\nconst loadSchema = function () {\n\tconst schema = fs.readFileSync(__dirname + '/schema.prisma', 'utf-8');\n\n\treturn schema;\n};\n\nmodule.exports = loadSchema;\n`;
-
-export { SCHEMA_SCRIPT };
