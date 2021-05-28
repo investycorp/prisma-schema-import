6a3d0af166d3aa85096e0dac9dbf6356b536e074
Chan Kang <me@kentakang.com>
2021-05-28 18:24:34 +0900

    fix: fix error on git repository is exists

--- src/git/index.ts
+++ src/git/index.ts
@@ -1,17 +1,32 @@
 import { execSync, ExecSyncOptions } from 'child_process';
 
+const isGitRepository = (directory: string) => {
+  try {
+    const execOptions: ExecSyncOptions = {
+      stdio: 'ignore',
+      cwd: directory,
+    };
+
+    execSync('git rev-parse --is-inside-work-tree', execOptions);
+  } catch (_) {
+    return false;
+  }
+};
+
 const gitInitialize = (directory: string) => {
   const execOptions: ExecSyncOptions = {
     stdio: 'ignore',
     cwd: directory,
   };
 
-  execSync(`cd ${directory}`, execOptions);
   execSync('git --version', execOptions);
-  execSync('git init', execOptions);
-  execSync('git checkout -b main', execOptions);
-  execSync('git add -A', execOptions);
-  execSync('git commit -m "Initial commit from prisma-schema-import"', execOptions);
+
+  if (isGitRepository(directory) === false) {
+    execSync('git init', execOptions);
+    execSync('git checkout -b main', execOptions);
+    execSync('git add -A', execOptions);
+    execSync('git commit -m "Initial commit from prisma-schema-import"', execOptions);
+  }
 };
 
 export default gitInitialize;
